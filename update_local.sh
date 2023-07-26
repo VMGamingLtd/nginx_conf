1455d85b0b8bf4a10b72cb658ac3293b276f2398
Milan.Bella <Milan.Bella@aomvsr.sk>
2023-07-26 07:58:16 +0200

    m

--- update_local.sh
+++ update_local.sh
@@ -2,4 +2,5 @@ set -xe
 cp certs/cert.key ../nginx/certs/
 cp certs/cert.pem ../nginx/certs/
 cp nginx.conf ../nginx/conf/
-cp www/index.html ../nginx/html/
+rm -rf ../nginx/html/*
+cp -r www/* ../nginx/html/
